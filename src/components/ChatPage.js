1e256e15af86c5739b0cf883784d5c46ba12f738
Pavel Potemkin <specialforbtsu@gmail.com>
2023-06-27 20:28:55 +0700

    message listener added

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -4,15 +4,21 @@ import ChatBody from './ChatBody';
 import ChatFooter from './ChatFooter';
 
 const ChatPage = ({ socket }) => {
-  return (
-    <div className="chat">
-      <ChatBar />
-      <div className="chat__main">
-        <ChatBody />
-        <ChatFooter socket={socket}/>
+    const [messages, setMessages] = useState([]);
+  
+    useEffect(() => {
+      socket.on('messageResponse', (data) => setMessages([...messages, data]));
+    }, [socket, messages]);
+  
+    return (
+      <div className="chat">
+        <ChatBar socket={socket} />
+        <div className="chat__main">
+          <ChatBody messages={messages} />
+          <ChatFooter socket={socket} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default ChatPage;
