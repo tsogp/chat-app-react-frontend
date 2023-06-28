717f51b9f8cd849686ab65532dabbd507d4bf6ea
Pavel Potemkin <specialforbtsu@gmail.com>
2023-06-29 04:57:13 +0700

    typing effect

--- src/components/ChatBody.js
+++ src/components/ChatBody.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const ChatBody = ({ messages }) => {
+const ChatBody = ({ messages, lastMessageRef }) => {
   const navigate = useNavigate();
 
   const handleLeaveChat = () => {
@@ -41,6 +41,7 @@ const ChatBody = ({ messages }) => {
         <div className="message__status">
           <p>Someone is typing...</p>
         </div>
+        <div ref={lastMessageRef} />
       </div>
     </>
   );
