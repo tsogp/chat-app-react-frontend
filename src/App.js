f2f9f7fd6d03fb1d8f090454ea8ee1973d967d81
Pavel Potemkin <specialforbtsu@gmail.com>
2023-06-29 19:06:49 +0700

    routes updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import ChatPage from './components/ChatPage';
+import ChatApp from './components/ChatApp';
 import socketIO from 'socket.io-client';
 
 const socket = socketIO.connect('http://localhost:4000');
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Routes>
           <Route path="/" element={<Home socket={socket} />}></Route>
-          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatApp socket={socket} />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
